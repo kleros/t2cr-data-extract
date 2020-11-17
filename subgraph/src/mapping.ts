aad073b9740198aca231b8a5eaf91d22a72f25ec
mtsalenc <mtsalenc@gmail.com>
2020-11-17 11:35:34 -0300

    fix: event handling errors

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,20 +1,27 @@
-import { log } from "@graphprotocol/graph-ts";
+import { log, BigInt } from "@graphprotocol/graph-ts";
 import { ArbitrableTokenList, RequestSubmitted, TokenStatusChange } from "../generated/ArbitrableTokenList/ArbitrableTokenList";
-import { Request } from "../generated/schema";
+import { Request, Token } from "../generated/schema";
 
 
-const PENDING = 'PENDING'
-const ACCEPTED = 'ACCEPTED'
-const REJECTED = 'REJECTED'
+const PENDING = 'Pending'
+const ACCEPTED = 'Accepted'
+const REJECTED = 'Rejected'
 
-const REGISTRATION = 'REGISTRATION'
-const REMOVAL = 'REMOVAL'
+const REGISTRATION = 'Registration'
+const REMOVAL = 'Removal'
 
 export function handleRequestSubmitted(event: RequestSubmitted): void {
-  let tcr = ArbitrableTokenList.bind(event.address)
-  let tokenInfo = tcr.getTokenInfo(event.params._tokenID)
-  let numberOfRequests = tokenInfo.value5
-  let id = event.params._tokenID.toHexString()+'-'+numberOfRequests.toString()
+  let token = Token.load(event.params._tokenID.toHexString())
+  let numberOfRequests = BigInt.fromI32(1)
+  if (token == null) {
+    token = new Token(event.params._tokenID.toHexString())
+    token.numberOfRequests = BigInt.fromI32(1)
+  } else {
+    token.numberOfRequests = token.numberOfRequests.plus(BigInt.fromI32(1))
+  }
+  token.save()
+
+  let id = event.params._tokenID.toHexString()+'-'+numberOfRequests.minus(BigInt.fromI32(1)).toString()
   let request = new Request(id)
 
   request.timestamp = event.block.timestamp
@@ -24,13 +31,14 @@ export function handleRequestSubmitted(event: RequestSubmitted): void {
     : REMOVAL
   request.requester = event.transaction.from
 
+  request.resolutionTime = BigInt.fromI32(0)
   request.save()
 }
 
 export function handleTokenStatusChange(event: TokenStatusChange): void {
-  if (event.params._status !== 0 || event.params._status !== 1)
-    return
-
+  if (event.params._status !== 0 || event.params._status !== 1) {
+    return // Request not yet resolved. Noop.
+  }
 
   let tcr = ArbitrableTokenList.bind(event.address)
   let tokenInfo = tcr.getTokenInfo(event.params._tokenID)
@@ -50,6 +58,7 @@ export function handleTokenStatusChange(event: TokenStatusChange): void {
     if (event.params._status === 0) request.result = ACCEPTED
     else request.result = REJECTED
   }
+
 
   request.save()
 }
